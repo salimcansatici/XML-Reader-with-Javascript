65934b487b995f252690a2e56f4ca323a24b9fd2
Salimcan Satıcı <salimcan@salimcan.com>
2020-04-05 01:45:54 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -13,6 +13,20 @@ document.getElementById("demo").addEventListener('click', function () {
     document.getElementById("demoalti").innerHTML = "XML Çek butonuna basabilirsiniz.";
     document.getElementById("xmlverininoldugu").innerHTML = "Demo Aktarıldı!";
     document.getElementById("xmlurlsinin").innerHTML = "Demo Aktarıldı!";
+
+});
+
+
+    document.getElementById("temizle").addEventListener('click', function () {
+    document.getElementById("xmlverininoldugu").innerHTML = "XML verinin olduğu URL'i giriniz.";
+    document.getElementById("xmlurlsinin").innerHTML = "XML Url'sinin içerisinde aradığınız TAG'ı yazınız";
+    document.getElementById("demoalti").innerHTML = "";
+    var xmlDataInputDemo = document.getElementById('xmlLink');
+    var xmlTagsDemo = document.getElementById('xmltags');
+    xmlDataInputDemo.value = '';
+    xmlTagsDemo.value = ''; //XmlTag
+
+
     
 });
 
